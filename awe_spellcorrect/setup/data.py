bf7c574bd85aefde315d9a91ab042c5aff924348
Piotr Mitros <piotr@mitros.org>
2022-10-07 10:22:38 -0400

    Download subwordbert only if it doesn't exist

--- awe_spellcorrect/setup/data.py
+++ awe_spellcorrect/setup/data.py
@@ -13,21 +13,23 @@ from setuptools.command.develop import develop
 from setuptools.command.develop import develop as _develop
 
 import os
+import os.path
 from pathlib import Path
 
 from distutils.sysconfig import get_python_lib
 import sysconfig
 
+
 class data:
-
-    def extra_install_commands(self):
-
-        # This is a datafile that neuspell needs that somehow got corrupted
-        # in its original location. We have a copy we're using to fix this
-        # problem at the location specified here. To fix once the issue
-        # is properly addressed.
+    def grab_subwordbert(self, vocabpath, modelpath):
+        '''
+        This is a datafile that neuspell needs that somehow got corrupted
+        in its original location. We have a copy we're using to fix this
+        problem at the location specified here. To fix once the issue
+        is properly addressed.
+        '''
         os.system('wget \
-            https://s3.amazonaws.com/mitros.org/p/ets/subwordbert.tar.gz')
+        https://s3.amazonaws.com/mitros.org/p/ets/subwordbert.tar.gz')
         os.system('tar -xzf subwordbert.tar.gz')
 
         # TO-DO: This definition of siteloc may not always work. We need
@@ -35,17 +37,28 @@ class data:
         # that the BERT application is expecting to find, no matter what kind
         # of virtual or real environment we have. Hopefully this bug gets
         # fixed upstream and this bit of hacky code can go away.
-        siteloc = sysconfig.get_paths()["purelib"]
+        os.makedirs(os.path.basename(modelpath), exist_ok=True)
+        Path("subwordbert-probwordnoise/model.pth.tar").rename(modelpath)
+        Path("subwordbert-probwordnoise/vocab.pkl").rename(vocabpath)
         os.remove('subwordbert.tar.gz')
-        datapath = siteloc + '/data/checkpoints/subwordbert-probwordnoise/'
-        if not os.path.isdir(datapath):
-            Path(datapath).mkdir(parents=True, exist_ok=True)
-        Path("subwordbert-probwordnoise/model.pth.tar").rename(
-            siteloc +
-            '/data/checkpoints/subwordbert-probwordnoise/model.pth.tar')
-        Path("subwordbert-probwordnoise/vocab.pkl").rename(
-            siteloc + '/data/checkpoints/subwordbert-probwordnoise/vocab.pkl')
         os.rmdir('subwordbert-probwordnoise')
+
+    def extra_install_commands(self):
+        '''
+        Grab extra data files. For now, `subwordbert`
+        '''
+        # Typically, something like:
+        # ~/.virtualenvs/venv/lib/python3.10/site-packages
+        siteloc = sysconfig.get_paths()["purelib"]
+        datapath = siteloc + '/data/checkpoints/subwordbert-probwordnoise/'
+        vocabpath = datapath + 'vocab.pkl'
+        modelpath = datapath + 'model.pth.tar'
+
+        if not os.path.exists(vocabpath) or not os.path.exists(vocabpath):
+            print("Missing data files. Grabbing:")
+            print(vocabpath)
+            print(modelpath)
+            self.grab_subwordbert(vocabpath, modelpath)
 
     def __init__(self, args):
         if args.install or args.develop:
